de06cddf5cc4c9a77b8bb1779e21e777c7daa4d5
avi <hi@avi.im>
2020-04-17 22:39:46 +0530

    make the first test pass

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/avinassh/monkey/token"
+
 type Lexer struct {
 	input        string
 	position     int
@@ -8,6 +10,30 @@ type Lexer struct {
 }
 
 func New(input string) *Lexer {
-	l := &Lexer{}
+	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	}
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
