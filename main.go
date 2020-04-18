e964235937ca3dfe2df2a3aa8c3216508b2b4542
avi <hi@avi.im>
2020-04-18 16:37:56 +0530

    add a toy REPL

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/avinassh/monkey/repl"
+)
+
+func main() {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		u.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
