9873927a4a75a35998595792916f62012def98ec
avi <hi@avi.im>
2020-04-18 16:16:49 +0530

    update tests for multichar tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,6 +6,42 @@ import (
 	"github.com/avinassh/monkey/token"
 )
 
+func TestNextTokenTwoCharTokens(t *testing.T) {
+	input := `10 == 10;
+10 != 9;
+`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
 func TestNextTokenMore(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
