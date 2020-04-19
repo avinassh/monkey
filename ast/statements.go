aa2f40b9c024392e1eb30b79c6084f91c62bff7e
avi <hi@avi.im>
2020-04-19 14:28:37 +0530

    Add return statements and tests

--- ast/statements.go
+++ ast/statements.go
@@ -24,3 +24,14 @@ func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
