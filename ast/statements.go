96b9a437b2f43d23533658f518705cec6c23f9ec
avi <hi@avi.im>
2020-04-19 18:47:30 +0530

    Add tests for integer literal expression

--- ast/statements.go
+++ ast/statements.go
@@ -86,3 +86,12 @@ func (rs *ReturnStatement) String() string {
 	out.WriteString(";")
 	return out.String()
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
