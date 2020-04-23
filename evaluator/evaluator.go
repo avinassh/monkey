8143043147735975e72834cd2a0e75098bfba17d
avi <hi@avi.im>
2020-04-23 20:49:16 +0530

    eval minus operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -42,6 +42,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -58,4 +60,12 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return nil
+	}
+	obj := right.(*object.Integer)
+	return &object.Integer{Value: -obj.Value}
 }
