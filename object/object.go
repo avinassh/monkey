815dcab692c98428f1d711050fce445f58f1abc1
avi <hi@avi.im>
2020-04-24 13:12:13 +0530

    Add RETURN_OBJ

--- object/object.go
+++ object/object.go
@@ -20,3 +20,8 @@ type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
+
+type ReturnValue struct{ Value Object }
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
