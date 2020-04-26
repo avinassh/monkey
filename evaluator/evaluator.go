eca0574c5a7c3a813829d842da3e30e5a860e347
avi <hi@avi.im>
2020-04-26 11:10:59 +0530

    eval builtin len fn

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -224,11 +224,13 @@ func evalMinusOperatorExpression(right object.Object) object.Object {
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
-	return val
+	if val, ok := builtins[node.Value]; ok {
+		return val
+	}
+	return newError("identifier not found: " + node.Value)
 }
 
 func evalFnLiteral(node *ast.FunctionLiteral, env *object.Environment) object.Object {
@@ -264,14 +266,16 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 // to stop the evaluation of the last called function’s body. That’s why we need unwrap it, so that evalBlockStatement
 // won’t stop evaluating statements in “outer” functions.
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
--- object/object.go
+++ object/object.go
@@ -68,3 +68,10 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
