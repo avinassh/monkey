d86ce5b650b25115080cac3bc3f0803e8f18aa28
avi <hi@avi.im>
2020-04-26 10:02:15 +0530

    string concat tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -223,6 +223,10 @@ if (10 > 1) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
