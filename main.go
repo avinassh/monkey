1f59c208d997289324eada3cdbb39d436704e5a1
avi <hi@avi.im>
2020-04-26 10:24:30 +0530

    call it M

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+	fmt.Printf("Hello %s! This is the M programming language!\n",
 		u.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
