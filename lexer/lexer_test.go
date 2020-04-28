8b16c97faf480ec92c5e57f821e69b8cda899a53
avi <hi@avi.im>
2020-04-28 20:09:59 +0530

    tokenise array brackets

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,6 +6,38 @@ import (
 	"github.com/avinassh/monkey/token"
 )
 
+func TestNextTokenArray(t *testing.T) {
+	input := `[1, 2];`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
 func TestNextTokenTwoCharTokens(t *testing.T) {
 	input := `10 == 10;
 10 != 9;
